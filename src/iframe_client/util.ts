4243f6c4616e8f51f917c1770662c5daed128518
StageDog <aksanajw845@gmail.com>
2024-12-22 22:41:46 +0800

    feat: 获取楼层 id 的辅助函数

--- src/iframe_client/util.ts
+++ src/iframe_client/util.ts
@@ -1,8 +1,30 @@
 /**
  * 获取 iframe 的名称
  *
- * @returns 对于楼层消息是 `message-楼层id-属于该楼层第几个代码块`; 对于全局脚本是 `script-脚本名称`
+ * @returns 对于楼层消息 iframe 是 `message-楼层id-属于该楼层第几个代码块`; 对于全局脚本 iframe 是 `script-脚本名称`
  */
 function getIframeName(): string {
   return (window.frameElement as Element).id;
+}
+
+/**
+ * 获取楼层消息 iframe 的所在楼层 id
+ *
+ * @returns 楼层消息 iframe 的所在楼层 id
+ */
+function getCurrentMessageId(): number {
+  const match = getIframeName().match(/^message-iframe-(\d+)-\d+$/);
+  if (!match) {
+    throw Error(`获取 iframe 所在楼层 id 时出错: 不要对全局脚本调用 getCurrentMessageId!`);
+  }
+  return parseInt(match.toString());
+}
+
+/**
+ * 获取最新楼层 id
+ *
+ * @returns 最新楼层id
+ */
+async function getLastMessageId(): Promise<number> {
+  return parseInt(await triggerSlashWithResult("/pass {{lastMessageId}}") as string);
 }
--- src_exported/iframe_client_exported/util.js
+++ src_exported/iframe_client_exported/util.js
@@ -2,10 +2,30 @@ export const iframe_client_util = `
 /**
  * 获取 iframe 的名称
  *
- * @returns 对于楼层消息是 \`message-楼层id-属于该楼层第几个代码块\`; 对于全局脚本是 \`script-脚本名称\`
+ * @returns 对于楼层消息 iframe 是 \`message-楼层id-属于该楼层第几个代码块\`; 对于全局脚本 iframe 是 \`script-脚本名称\`
  */
 function getIframeName() {
     return window.frameElement.id;
+}
+/**
+ * 获取楼层消息 iframe 的所在楼层 id
+ *
+ * @returns 楼层消息 iframe 的所在楼层 id
+ */
+function getCurrentMessageId() {
+    const match = getIframeName().match(/^message-iframe-(\d+)-\d+$/);
+    if (!match) {
+        throw Error(\`获取 iframe 所在楼层 id 时出错: 不要对全局脚本调用 getCurrentMessageId!\`);
+    }
+    return parseInt(match.toString());
+}
+/**
+ * 获取最新楼层 id
+ *
+ * @returns 最新楼层id
+ */
+async function getLastMessageId() {
+    return parseInt(await triggerSlashWithResult("/pass {{lastMessageId}}"));
 }
 `;
 //# sourceMappingURL=util.js.map
