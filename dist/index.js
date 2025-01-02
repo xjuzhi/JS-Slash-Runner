0045fd874ac4147eef8b3b3a91848a85a4b2fbbc
StageDog <aksanajw845@gmail.com>
2025-01-02 23:18:55 +0800

    feat: 自动关闭禁用外部媒体

--- dist/index.js
+++ dist/index.js
@@ -788,9 +788,11 @@ async function onAutoEnableCharacterRegexClick() {
 async function autoDisableIncompatibleOptions() {
     if (power_user.auto_fix_generated_markdown || power_user.trim_sentences) {
         power_user.auto_fix_generated_markdown = false;
-        power_user.trim_sentences = false;
         $("#auto_fix_generated_markdown").prop("checked", power_user.auto_fix_generated_markdown);
+        power_user.trim_sentences = false;
         $("#trim_sentences_checkbox").prop("checked", power_user.trim_sentences);
+        power_user.forbid_external_media = false;
+        $("#forbid_external_media").prop("checked", power_user.forbid_external_media);
     }
     saveSettingsDebounced();
 }
