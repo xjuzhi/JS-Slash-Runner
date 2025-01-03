ed1984dda5c615e9980a7035ee094362b95c34f4
StageDog <aksanajw845@gmail.com>
2025-01-04 00:19:10 +0800

    feat: 更改世界书条目时刷新对应显示

--- src/iframe_server/lorebook.ts
+++ src/iframe_server/lorebook.ts
@@ -223,6 +223,14 @@ function assignFieldValuesToWiEntry(data: any, wi_entry: any, field_values: any)
     });
 }
 
+function reload_editor(file: string, load_if_not_selected: boolean = false): void {
+  const currentIndex = Number($('#world_editor_select').val());
+  const selectedIndex = world_names.indexOf(file);
+  if (selectedIndex !== -1 && (load_if_not_selected || currentIndex === selectedIndex)) {
+    $('#world_editor_select').val(selectedIndex).trigger('change');
+  }
+}
+
 const event_handlers = {
   iframe_get_lorebook_settings: async (event: MessageEvent<IframeGetLorebookSettings>): Promise<void> => {
     const iframe_name = getIframeName(event);
@@ -395,6 +403,7 @@ const event_handlers = {
 
     await Promise.all(entries.map(process_entry));
     await saveWorldInfo(lorebook, data);
+    reload_editor(lorebook);
 
     console.info(`[Lorebook][setLorebookEntries](${iframe_name}) 修改世界书 '${lorebook}' 中以下条目的一些字段: ${JSON.stringify(entries)}`);
   },
@@ -419,6 +428,7 @@ const event_handlers = {
       { targetOrigin: "*" }
     );
     await saveWorldInfo(lorebook, data);
+    reload_editor(lorebook);
 
     console.info(`[Lorebook][createLorebookEntry](${iframe_name}) 在世界书 '${lorebook}' 中新建 uid='${wi_entry.uid}' 条目, 并设置内容: ${JSON.stringify(field_values)}`);
   },
@@ -449,6 +459,7 @@ const event_handlers = {
       // @ts-ignore 2345
       deleteWIOriginalDataValue(data, lorebook_uid);
       await saveWorldInfo(lorebook, data);
+      reload_editor(lorebook);
     }
 
     console.info(`[Lorebook][deleteLorebookEntry](${iframe_name}) 删除世界书 '${lorebook}' 中的 uid='${lorebook_uid}' 条目${deleted ? '成功' : '失败'}`);
