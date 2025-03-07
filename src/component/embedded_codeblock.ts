72775c1edee8353bc0e728d07bf5d62577844b3f
StageDog <aksanajw845@gmail.com>
2025-03-08 02:36:54 +0800

    fix: 将内嵌代码块的执行移到 SETTINGS_READY 上，避免执行多次

--- src/component/embedded_codeblock.ts
+++ src/component/embedded_codeblock.ts
@@ -2,8 +2,8 @@ import { event_types, eventSource } from '../../../../../../script.js';
 
 const codeblock_regex = /`{5}javascript(.+?)`{5}/gs;
 
-async function parse_message(event_data: Parameters<ListenerType['chat_completion_prompt_ready']>[0]): Promise<void> {
-  for (const message of event_data.chat) {
+async function parse_message(event_data: Parameters<ListenerType['chat_completion_settings_ready']>[0]): Promise<void> {
+  for (const message of event_data.messages) {
     try {
       message.content = await parse_codeblock(message.content);
     } catch (error) {
@@ -22,15 +22,16 @@ async function parse_codeblock(content: string): Promise<string> {
   }
   for (const match of matches) {
     const fn = new Function(match[1]);
-    content = content.replace(match[0], String(await fn()));
+    const result = await fn();
+    content = content.replace(match[0], typeof result === 'string' ? result : '');
   }
   return content;
 }
 
 export function initializeEmbeddedCodeblockOnExtension() {
-  eventSource.on(event_types.CHAT_COMPLETION_PROMPT_READY, parse_message);
+  eventSource.on(event_types.CHAT_COMPLETION_SETTINGS_READY, parse_message);
 }
 
 export function destroyEmbeddedCodeblockOnExtension() {
-  eventSource.removeListener(event_types.CHAT_COMPLETION_PROMPT_READY, parse_message);
+  eventSource.removeListener(event_types.CHAT_COMPLETION_SETTINGS_READY, parse_message);
 }
