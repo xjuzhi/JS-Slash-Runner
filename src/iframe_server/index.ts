c70223e3453a781be0937c67cabae91d0c30a4ae
StageDog <aksanajw845@gmail.com>
2025-03-24 23:08:11 +0800

    fix: 解决循环依赖

--- src/iframe_server/index.ts
+++ src/iframe_server/index.ts
@@ -11,6 +11,38 @@ import { registerIframeTavernRegexHandler } from '@/iframe_server/tavern_regex';
 import { registerIframeUtilHandler } from '@/iframe_server/util';
 import { registerIframeVariableHandler } from '@/iframe_server/variables';
 
+import { IframeMessage, getLogPrefix, iframe_handlers } from '@/iframe_server/_impl';
+import { t } from '@sillytavern/scripts/i18n';
+
+export async function handleIframe(event: MessageEvent<IframeMessage>): Promise<void> {
+  if (!event.data) return;
+
+  const handler = iframe_handlers[event.data.request];
+  if (!handler) {
+    return;
+  }
+
+  let result: any = undefined;
+  try {
+    result = await handler(event);
+  } catch (err) {
+    const error = err as Error;
+    toastr.error(t`${getLogPrefix(event)}${error.name + ': ' + error.message}${error.stack ? error.stack : ''}`);
+    console.error(getLogPrefix(event), error);
+  } finally {
+    (event.source as MessageEventSource).postMessage(
+      {
+        request: event.data.request + '_callback',
+        uid: event.data.uid,
+        result: result,
+      },
+      {
+        targetOrigin: '*',
+      },
+    );
+  }
+}
+
 registerIframeChatMessageHandler();
 registerIframeDisplayedMessageHandler();
 registerIframeFrontendVersionHandler();
