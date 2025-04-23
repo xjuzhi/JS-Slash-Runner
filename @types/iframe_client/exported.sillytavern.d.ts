50b77a5e409d731fec8cdb7b025bc77d528ae18c
StageDog <aksanajw845@gmail.com>
2025-04-23 13:18:59 +0800

    docs: 补充 EjsTemplate

--- @types/iframe_client/exported.sillytavern.d.ts
+++ @types/iframe_client/exported.sillytavern.d.ts
@@ -1,10 +1,4 @@
 /**
- * 酒馆助手提供的额外功能, 具体内容见于 https://n0vi028.github.io/JS-Slash-Runner-Doc
- * 你也可以在酒馆页面按 f12, 在控制台中输入 `window.TavernHelper` 来查看当前酒馆助手所提供的接口
- */
-const TavernHelper: typeof window.TavernHelper;
-
-/**
  * 酒馆提供给插件的稳定接口, 具体内容见于 SillyTavern/public/scripts/st-context.js 或 https://github.com/SillyTavern/SillyTavern/blob/release/public/scripts/st-context.js
  * 你也可以在酒馆页面按 f12, 在控制台中输入 `window.SillyTavern.getContext()` 来查看当前酒馆所提供的接口
  */
--- src/iframe_client/exported.ts
+++ src/iframe_client/exported.ts
@@ -11,10 +11,15 @@ const SillyTavern = (
 /**
  * 酒馆助手提供的额外功能, 具体内容见于 https://n0vi028.github.io/JS-Slash-Runner-Doc
  * 你也可以在酒馆页面按 f12, 在控制台中输入 `window.TavernHelper` 来查看当前酒馆助手所提供的接口
- * TODO: 怎么导出 TavernHelper 的类型声明
  */
 const TavernHelper = (window.parent as unknown as { TavernHelper: Record<string, any> }).TavernHelper;
 
 for (const key in TavernHelper) {
   (window as any)[key] = TavernHelper[key];
 }
+
+/**
+ * 提示词模板语法插件所提供的额外功能, 具体内容见于 https://github.com/zonde306/ST-Prompt-Template
+ * 你也可以在酒馆页面按 f12,在控制台中输入 `window.EjsTemplate` 来查看当前提示词模板语法所提供的接口
+ */
+const EjsTemplate = (window.parent as unknown as { EjsTemplate: Record<string, any> }).EjsTemplate;
