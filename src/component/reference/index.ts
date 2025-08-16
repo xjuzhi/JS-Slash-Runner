924744bffedbf89ba84d7e86c4ae0f9b625c310d
N0VI028 <Hardware9939@proton.me>
2025-08-16 21:54:26 +0800

    style: 修改编写参考的样式

--- src/component/reference/index.ts
+++ src/component/reference/index.ts
@@ -1,9 +1,14 @@
+import { callGenericPopup, POPUP_TYPE } from '@sillytavern/scripts/popup';
 import {
   SlashCommandArgument,
   SlashCommandNamedArgument,
 } from '@sillytavern/scripts/slash-commands/SlashCommandArgument';
 import { SlashCommandParser } from '@sillytavern/scripts/slash-commands/SlashCommandParser';
 
+/**
+ * 格式化酒馆 STScript 命令列表
+ * @returns 格式化后的酒馆 STScript 命令列表
+ */
 function formatSlashCommands(): string {
   const cmdList = Object.keys(SlashCommandParser.commands)
     .filter(key => SlashCommandParser.commands[key].name === key) // exclude aliases
@@ -66,11 +71,54 @@ function formatSlashCommands(): string {
     .join('\n');
 }
 
-export async function initReference() {
-  $('#download_slash_commands').on('click', function () {
+/**
+ * 打开查看文档popup
+ */
+function openViewDocsPopup() {
+  const $html = $(`
+  <div class="flex-container flexFlowColumn">
+  <a href="https://n0vi028.github.io/JS-Slash-Runner-Doc/guide/基本用法/如何正确使用酒馆助手.html" target="_blank" class="menu_button interactable width100p">
+    查看酒馆助手帮助文档
+  </a>
+  <a href="https://rentry.org/sillytavern-script-book" target="_blank" class="menu_button interactable width100p">
+    查看 STScript 命令手册中文版
+  </a>
+  </div>
+  `);
+  callGenericPopup($html, POPUP_TYPE.DISPLAY);
+}
+
+/**
+ * 打开下载参考文件popup
+ */
+function openDownloadReferencePopup() {
+  const $html = $(`
+  <div class="flex-container flexFlowColumn">
+  <a href="https://gitlab.com/novi028/JS-Slash-Runner/-/raw/main/dist/@types.zip?ref_type=heads&inline=false" target="_blank" class="menu_button interactable width100p">
+    下载酒馆助手类型声明文件 (压缩包)
+  </a>
+  <a href="https://gitlab.com/novi028/JS-Slash-Runner/-/raw/main/dist/@types.txt?ref_type=heads&inline=false" target="_blank" class="menu_button interactable width100p">
+    下载酒馆助手类型声明文件 (单文件)
+  </a>
+  <div class="menu_button interactable width100p" id="download_slash_commands">
+    下载文本版酒馆 STScript 命令大全
+  </div>
+  </div>
+  `);
+  $html.find('#download_slash_commands').on('click', function () {
     const url = URL.createObjectURL(new Blob([formatSlashCommands()], { type: 'text/plain' }));
-    $(this).attr('href', url);
-    $(this).attr('download', 'slash_command.txt');
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'slash_command.txt';
+    a.click();
     setTimeout(() => URL.revokeObjectURL(url), 0);
   });
+  callGenericPopup($html, POPUP_TYPE.DISPLAY);
+}
+/**
+ * 初始化参考
+ */
+export async function initReference() {
+  $('#view_tavern_helper_docs').on('click', openViewDocsPopup);
+  $('#download_tavern_helper_types').on('click', openDownloadReferencePopup);
 }
