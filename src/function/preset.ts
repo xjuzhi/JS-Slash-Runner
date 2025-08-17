966a1d2c928451139aea03b4b750b02ec555956d
StageDog <aksanajw845@gmail.com>
2025-08-17 20:28:25 +0800

    fix: 修复预设注入相关字段的获取

--- src/function/preset.ts
+++ src/function/preset.ts
@@ -299,6 +299,7 @@ type _OriginalPlaceholderPrompt = {
   injection_order: number;
 
   role: 'system' | 'user' | 'assistant';
+  content?: never;
 
   system_prompt: true;
   marker?: true;
@@ -331,7 +332,7 @@ function toPresetPrompt(prompt: _OriginalPrompt, prompt_order: _OriginalPromptOr
 
   if (is_normal_prompt || is_placeholder_prompt) {
     result = result.set('position.type', { 0: 'relative', 1: 'in_chat' }[prompt.injection_position ?? 0]);
-    if (prompt.injection_depth === 1) {
+    if (prompt.injection_position === 1) {
       result = result.set('position.depth', prompt.injection_depth ?? 4);
       result = result.set('position.order', prompt.injection_order ?? 100);
     }
