3139071509be5c19fbde19c4a9ef3a48f5228284
StageDog <aksanajw845@gmail.com>
2025-09-09 11:04:01 +0800

    fix: 明确没获取到 debug.enabled 时默认为 false

--- src/component/script_repository/script_controller.ts
+++ src/component/script_repository/script_controller.ts
@@ -35,7 +35,7 @@ class ScriptExecutor {
         destroyIframe(iframeElement);
       }
 
-      const is_debug_enabled = getSettingValue('debug.enabled');
+      const is_debug_enabled = getSettingValue('debug.enabled', false);
       const htmlContent = this.createScriptHtml(script, is_debug_enabled);
 
       const $iframe = $('<iframe>', {
